9b8ffc50920b7060cce9c66c31737f577606866c
GosiaWitkowska <gosias13@gmail.com>
2023-08-25 18:57:39 +0100

    sdfsfd

---
+++ js/loadEssencials.js
@@ -0,0 +1,41 @@
+document.write('<link rel="stylesheet" type="text/css" href="css/style.css" />');
+document.write('<script type="text/javascript" src="js/jquery-1.11.1.min.js" ></script>');
+
+var element = document.getElementById('mainFooter');
+element.classList.add("section");
+element.classList.add("footer-classic");
+element.classList.add("section-sm");
+document.getElementById('mainFooter').innerHTML =
+"<div class='container'>"
+         +"<div class='row row-30'>"
+         +"   <div class='col-lg-3 wow fadeInLeft'>"
+         +"     <!--Brand--><a class='brand' href='index.html'>"
++"			  <P class='footer-classic-title'>Choose Frequency</p>"
++"              <p class='footer-classic-description offset-top-0 offset-right-25'>"
++"			  Choose Frequency provides ideas and ways to change the frequency you possess to raise vibrations and attract new and more appealing experiences to your life.</p>"
++"            </div>"
++"            <div class='col-lg-3 col-sm-8 wow fadeInUp'>"
++"              <P class='footer-classic-title'>contact info</P>"
++"              <div class='d-block offset-top-0'><span class='d-lg-block'>London, UK</span></div><a class='d-inline-block accent-link' href='mailto:#'>choosefrequency@gmail.com</a>"
++"			  <a class='d-inline-block' href='tel:#'>+44 7758748210</a>"
++"            </div>"
++"            <div class='col-lg-2 col-sm-4 wow fadeInUp' data-wow-delay='.3s'>"
++"              <P class='footer-classic-title'>Quick Links</P>"
++"              <ul class='footer-classic-nav-list'>"
++"                <li><a href='index.html'>Home</a></li>"
++"                <li><a href='about.html'>About</a></li>"
++"                <li><a href='contacts.html'>Contact</a></li>"
++"              </ul>"
++"            </div>"
++"          </div>"
++"        </div>"
++"	<div class='container wow fadeInUp' data-wow-delay='.4s'>"
++"          <div class='footer-classic-aside'>"
++"            <p class='rights'><span>&copy;&nbsp;</span><span class='copyright-year'></span>. All Rights Reserved.</p>"
++"            <ul class='social-links'>"
++"				<li><a class='icon icon-sm icon-circle icon-circle-md icon-bg-white fa-youtube-play' href='https://www.youtube.com/@choosefrequency' target='_blank'></a></li>"
++"                <li><a class='icon icon-sm icon-circle icon-circle-md icon-bg-white fa-facebook' href='https://www.facebook.com/choosefrequency' target='_blank'></a></li>"
++"                <li><a class='icon icon-sm icon-circle icon-circle-md icon-bg-white fa-instagram' href='https://www.instagram.com/choosefrequency/' target='_blank'></a></li>"			
++"            </ul>"
++"          </div>"
++"        </div>";	
