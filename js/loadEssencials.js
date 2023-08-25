52324445c8d682051551229420c7dd29273a4be4
GosiaWitkowska <gosias13@gmail.com>
2023-08-25 19:35:44 +0100

    dfg

--- js/loadEssencials.js
+++ js/loadEssencials.js
@@ -5,7 +5,7 @@ var element = document.getElementById('mainFooter');
 element.classList.add("section");
 element.classList.add("footer-classic");
 element.classList.add("section-sm");
-document.getElementById('mainFooter').innerHTML =
+element.innerHTML =
 "<div class='container'>"
          +"<div class='row row-30'>"
          +"   <div class='col-lg-3 wow fadeInLeft'>"
@@ -39,3 +39,93 @@ document.getElementById('mainFooter').innerHTML =
 +"            </ul>"
 +"          </div>"
 +"        </div>";	
+
+
+var element = document.getElementById('mainHeader');
+element.classList.add("section");
+element.classList.add("page-header");
+element.innerHTML =
+"<!--RD Navbar-->"
++"<div class='rd-navbar-wrap'>"
++"  <nav class='rd-navbar rd-navbar-classic' data-layout='rd-navbar-fixed' data-sm-layout='rd-navbar-fixed' data-md-layout='rd-navbar-fixed' data-md-device-layout='rd-navbar-fixed' data-lg-layout='rd-navbar-static' data-lg-device-layout='rd-navbar-static' data-xl-layout='rd-navbar-static' data-xl-device-layout='rd-navbar-static' data-lg-stick-up-offset='46px' data-xl-stick-up-offset='46px' data-xxl-stick-up-offset='46px' data-lg-stick-up='true' data-xl-stick-up='true' data-xxl-stick-up='true'>"
++"	<div class='rd-navbar-collapse-toggle rd-navbar-fixed-element-1' data-rd-navbar-toggle='.rd-navbar-collapse'><span></span></div>"
++"	<div class='rd-navbar-aside-outer rd-navbar-collapse bg-gray-dark'>"
++"	  <div class='rd-navbar-aside'>"
++"		<ul class='list-inline navbar-contact-list'>"
++"		  <li>"
++"			<div class='unit unit-spacing-xs align-items-center'>"
++"			  <div class='unit-left'><span class='icon text-middle fa-phone'></span></div>"
++"			  <div class='unit-body'><a href='tel:#'>+44 07758748210</a></div>"
++"			</div>"
++"		  </li>"
++"		  <li>"
++"			<div class='unit unit-spacing-xs align-items-center'>"
++"			  <div class='unit-left'><span class='icon text-middle fa-envelope'></span></div>"
++"			  <div class='unit-body'><a href='mailto:#'>choosefrequency@gmail.com</a></div>"
++"			</div>"
++"		  </li>"
++"		  <li>"
++"			<div class='unit unit-spacing-xs align-items-center'>"
++"			  <div class='unit-left'><span class='icon text-middle fa-map-marker'></span></div>"
++"			  <div class='unit-body'><a href='#'>London UK</a></div>"
++"			</div>"
++"		  </li>"
++"		</ul>"
++"		<a class='button button-190 button-circle btn-primary-rounded' href='index.html#pricing'>book session</a>"
++"		<ul class='social-links'>"
++"		  <li><a class='icon icon-sm icon-circle icon-circle-md icon-bg-white fa-youtube-play' href='https://www.youtube.com/@choosefrequency' target='_blank'></a></li>"
++"		  <li><a class='icon icon-sm icon-circle icon-circle-md icon-bg-white fa-facebook' href='https://www.facebook.com/choosefrequency' target='_blank'></a></li>"
++"		  <li><a class='icon icon-sm icon-circle icon-circle-md icon-bg-white fa-instagram' href='https://www.instagram.com/choosefrequency/' target='_blank'></a></li>"
++"		</ul>"
++"	  </div>"
++"	</div>"
++"	<div class='rd-navbar-main-outer'>"
++"	  <div class='rd-navbar-main'>"
++"		<!--RD Navbar Panel-->"
++"		<div class='rd-navbar-panel'>"
++"		  <!--RD Navbar Toggle-->"
++"		  <button class='rd-navbar-toggle' data-rd-navbar-toggle='.rd-navbar-nav-wrap'><span></span></button>"
++"		  <div class='book_small'>"
++"			<a class='button button-circle btn-primary-rounded' href='index.html#pricing'>book session</a>"
++"		  </div>"
++"		</div>"
++"		<div class='rd-navbar-main-element'>"
++"		  <div class='rd-navbar-nav-wrap'>"
++"			<ul class='rd-navbar-nav'>"
++"			  <li id='menu-home' class='rd-nav-item'><a class='rd-nav-link' href='index.html'>Home</a>"
++"			  </li>"
++"			  <li id='menu-about' class='rd-nav-item'><a class='rd-nav-link' href='about.html'>About</a>"
++"			  </li>"
++"			  <!--<li class='rd-nav-item'><a class='rd-nav-link' href='https://drive.google.com/file/d/1ZpGNZkO8bej1v293Ttz4eq50MPqXrPXr/view?usp=share_link' target='_blank'>Book</a></li>-->"
++"			  <li id='menu-blog' class='rd-nav-item'><a class='rd-nav-link' href='blog.html' target='_blank'>Blog</a></li>"
++"			  <li class='rd-nav-item'><a class='rd-nav-link' href='https://www.youtube.com/@choosefrequency'>YouTube</a></li>"
++"			  <li id='menu-contact' class='rd-nav-item'><a class='rd-nav-link' href='contacts.html'>Contact</a></li>"
++"			  <li id='menu-collaboration' class='rd-nav-item'><a class='rd-nav-link' href='collaboration.html'>Collaboration</a></li>"
++"			</ul>"
++"		  </div>"
++"		</div>"
++"	  </div>"
++"	</div>"
++"  </nav>"
++"</div>";
+
+if (element.classList.contains("header-blog")) {
+	var element = document.getElementById('menu-blog');
+	element.classList.add("active");
+}
+if (element.classList.contains("header-about")) {
+	var element = document.getElementById('menu-about');
+	element.classList.add("active");
+}
+if (element.classList.contains("header-contact")) {
+	var element = document.getElementById('menu-contact');
+	element.classList.add("active");
+}
+if (element.classList.contains("header-collaboration")) {
+	var element = document.getElementById('menu-collaboration');
+	element.classList.add("active");
+}
+if (element.classList.contains("header-home")) {
+	var element = document.getElementById('menu-home');
+	element.classList.add("active");
+}
