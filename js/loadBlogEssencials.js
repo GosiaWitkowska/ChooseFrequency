85bcd2088e7d94cb8bdf30494991bb1ff8235081
GosiaWitkowska <gosias13@gmail.com>
2023-09-11 22:54:19 +0100

    asd

--- js/loadBlogEssencials.js
+++ js/loadBlogEssencials.js
@@ -1,8 +1,5 @@
 document.write('<title>Choose Frequency</title>');
 
-document.write('<meta charset="utf-8">');
-document.write('<meta name="viewport" content="width=device-width, height=device-height, initial-scale=1.0">');
-document.write('<meta http-equiv="X-UA-Compatible" content="IE=edge">');
 document.write('<link rel="stylesheet" type="text/css" href="../css/style.css" />');
 document.write('<link rel="icon" href="../images/CF_LogoCircle.png" type="image/x-icon">');
 document.write('<link rel="stylesheet" type="text/css" href="../fonts/fontMonterrat.css">');
@@ -10,8 +7,7 @@ document.write('<link rel="stylesheet" href="../css/bootstrap.css">');
 document.write('<link rel="stylesheet" href="../css/fonts.css">');
 document.write('<link rel="stylesheet" href="../css/style.css">');
 document.write('<script type="text/javascript" src="../js/jquery-1.11.1.min.js" ></script>');
-document.write('<script src="../js/core.min.js"></script>');
-document.write('<script src="../js/script.js"></script>');
+
 $(document).ready(function(){
 
 	var path = window.location.pathname;
