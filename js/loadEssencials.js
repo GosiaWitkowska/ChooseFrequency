c2daf7d80052477a98b02899b11cdca8cd556191
GosiaWitkowska <gosias13@gmail.com>
2023-12-23 08:41:42 +0000

    \zxx

--- js/loadEssencials.js
+++ js/loadEssencials.js
@@ -100,7 +100,7 @@ element.innerHTML =
 +"			  </li>"
 +"			  <li id='menu-blog' class='rd-nav-item'><a class='rd-nav-link' href='blog.html' target='_blank'>Blog</a></li>"
 +"			  <li class='rd-nav-item'><a class='rd-nav-link' href='https://www.youtube.com/@choosefrequency'>YouTube</a></li>"
-+"			  <li id='menu-books' class='rd-nav-item'><a class='rd-nav-link' href='books.html' target='_blank'>Book</a></li>" 
++"			  <li id='menu-books' class='rd-nav-item'><a class='rd-nav-link' href='book.html' target='_blank'>Book</a></li>" 
 +"			  <li id='menu-contact' class='rd-nav-item'><a class='rd-nav-link' href='contacts.html'>Contact</a></li>"
 +"			  <li id='menu-collaboration' class='rd-nav-item'><a class='rd-nav-link' href='collaboration.html'>Collaboration</a></li>"
 +"			  <li id='menu-sessions' class='rd-nav-item'><a class='rd-nav-link' href='sessions.html'>Sessions</a></li>"
